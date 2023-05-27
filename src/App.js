9ed225cc1be507f04bd0c6d54bd80c0829f13f2c
armel50 <armelfli50@gmail.com>
2023-05-27 10:45:14 -0500

    added dpo pay api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,91 @@
 
+import { useState, useEffect } from "react";
+import { origin , DPO_services} from "./constants/constants";
+import uuid from "react-uuid";
 function App() {
+
+  const {REACT_APP_DPO_API,REACT_APP_DPO_TOKEN, REACT_APP_DPO_COMP_REF} = process.env
+  const [payment_request, setPayment] = useState({
+    "email": "",
+    "first_name": "", 
+    "last_name": "", 
+    "address_line_1": "", 
+    "address_line_2": "",
+    "city": "", 
+    "country": "", 
+    "zip_or_postal": "", 
+    "country_code": "", 
+    "mobile": "",
+    "payment_date": new Date(), 
+    "description": "", 
+    "order_ref": "", 
+    "accounting_ref": "", 
+    "currency": "", 
+    "total": "", 
+    "payment_type": "mobile",
+    "card_holder_name": "", 
+    "card_number": "", 
+    "expiry_date": "", 
+    "security_code": "",
+    "mobile_money": ""
+
+  })
+
+  const DPO_pay= async ()=>{
+    const headers = {
+                      "Content-Type": "application/xml", 
+                      "Accept":"application/xml",
+                      "Access-Control-Allow-Origin": "*"
+                    }
+    const xml = `<?xml version="1.0" encoding="utf-8"?>
+                  <API3G>
+                    <CompanyToken>${REACT_APP_DPO_TOKEN}</CompanyToken>
+                    <Request>createToken</Request>
+                    <Transaction>
+                      <PaymentAmount>1.00</PaymentAmount>
+                      <PaymentCurrency>USD</PaymentCurrency>
+                      <CompanyRef>${REACT_APP_DPO_COMP_REF}</CompanyRef>
+                      <RedirectURL>${origin}</RedirectURL>
+                      <BackURL>${origin}</BackURL>
+                      <CompanyRefUnique>${uuid()}</CompanyRefUnique>
+                      <PTL>5</PTL>
+                    </Transaction>
+                    <Services>
+                      <Service>
+                        <ServiceType>${DPO_services.test_product}</ServiceType>
+                        <ServiceDescription>${payment_request.description}</ServiceDescription>
+                        <ServiceDate>${new Date()}</ServiceDate>
+                      </Service>
+                    </Services>
+                  </API3G>`
+    
+   fetch(REACT_APP_DPO_API, {
+      method: 'POST', 
+      mode: 'no-cors',
+      headers: headers, 
+      body:  xml
+    })
+    .then(rsp => console.log(rsp))
+   
+
+    
+    
+
+  }
+  useEffect(()=> {
+    DPO_pay()
+  })
+  
+  
   return (
+    <>
+
     <div className="App">
-      <h1>Hello world</h1>
+
+      <h1>Hello</h1>
     </div>
+    </>
+    
   );
 }
 
---
+++ src/constants/constants.js
@@ -0,0 +1,17 @@
+export var origin = document.location.origin
+export var DPO_services = {
+    "acoomodatoin": "3165",
+    "laptop_purchase": "3790",
+    "ecommerce": "3854", 
+    "test_service": "3978",
+    "ticket": "4622",
+    "school_fees": "5291",
+    "test_product": "5525",
+    "food_and_beverage": "5854",
+    "dpo_card_load": "46096",
+    "plumbing": "58224",
+    "flight": "69836",
+    "hotel": "69837"
+
+
+}
